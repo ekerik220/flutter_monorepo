55061f3dce0f9a32c411c1a98cbfe41cb527481e
Evan Kerik <ekerik220@gmail.com>
2022-12-04 14:22:06 +0900

    add widget to shared to test workflow

---
+++ shared/lib/test_widget.dart
@@ -0,0 +1,11 @@
+import 'package:flutter/src/widgets/container.dart';
+import 'package:flutter/src/widgets/framework.dart';
+
+class TestWidget extends StatelessWidget {
+  const TestWidget({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Container();
+  }
+}
